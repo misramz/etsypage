bca713182848b0b0278e2d8127ed74e79e1eeb6e
Nisa Sanyang <misramz@yahoo.com>
2016-06-06 09:48:33 -0400

    my icons wont hover

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,31 @@
+import { items } from './vendor';
 import $ from 'jquery';
 
-let url = 'https://api.etsy.com/v2/listings/active?api_key=h9oq2yf3twf4ziejn10b717i&keywords=whiskey&includes=Images,Shop';
 
-let info = $.getJSON(url);
-info.then(function(res){
-  console.log(res);
+
+
+var products = items.results;
+
+// console.log('prod: ', prod);
+
+var html = $('.images');
+
+products.forEach( function (x) {
+  var prodTem = `
+     <div class='hold'>
+       <img class='icon1' src='http://i.imgur.com/mspWLsN.png'>
+       <img class='icon2' src='http://i.imgur.com/4U7GDVn.png'>
+       <img class='prod' src='${x.Images[0].url_170x135}'>
+       <div class='info'>
+       <p class='titl'> ${x.title} </p>
+       <span class='ln'> ${x.Shop.login_name} </span>
+       <span class='price'> $${x.price} </span>
+       </div>
+     </div>
+
+       `;
+
+       console.log(prodTem);
+
+  html.append(prodTem);
 });
-
-
-
-let items = $('.items');
